7188edbd85ceb70b461baf3f1606858d4914c38f
Agustin Servin <agustinservin1@gmail.com>
2022-11-29 08:16:06 -0300

    subo archivos y estructura principal de mi portflio en desarrollo

---
+++ menu.js
@@ -0,0 +1,9 @@
+
+function mostrarmenu(){
+    var menu = document.getElementById('menu');
+    menu.style.left="0";
+}
+function cerrarmenu(){ 
+    var menu = document.getElementById('menu');
+    menu.style.left="-110%";
+}
