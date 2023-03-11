ec86bb0bc79d8335d3645b01b0d5c3112895e5ee
Agustin Servin Müller <agustinservin1@gmail.com>
2023-03-11 11:24:42 -0300

    avanzando en el proyecto, funcionalidad darkMode

--- menu.js
+++ menu.js
@@ -7,3 +7,10 @@ function cerrarmenu(){
     var menu = document.getElementById('menu');
     menu.style.left="-110%";
 }
+//funcion para darkmode
+const btnSwitch = document.getElementById('switch');
+btnSwitch.addEventListener("click", function(e){ 
+    //si tiene la clase dark se la quita y si no la tiene se la pone
+    document.body.classList.toggle('light');
+    btnSwitch.classList.toggle('active');   
+});
