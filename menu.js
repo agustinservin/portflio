34f55247512b532ac07f9dffc2c6f5c1154deb0b
agustin servin <agustinservin1@gmail.com>
2023-03-12 03:34:51 -0300

    funcionalidad preloader terminada

--- menu.js
+++ menu.js
@@ -1,12 +1,15 @@
-
+menuAbierto = false;
 function mostrarmenu(){
     var menu = document.getElementById('menu');
     menu.style.left="0";
+    menuAbierto =true;
 }
 function cerrarmenu(){ 
     var menu = document.getElementById('menu');
     menu.style.left="-110%";
+    menuAbierto= false;
 }
+
 //funcion para darkmode
 const btnSwitch = document.getElementById('switch');
 btnSwitch.addEventListener("click", function(e){ 
@@ -14,3 +17,11 @@ btnSwitch.addEventListener("click", function(e){
     document.body.classList.toggle('light');
     btnSwitch.classList.toggle('active');   
 });
+  window.onload = function() {
+    console.log("Window is loaded!");
+    var loader = document.getElementById('loader');
+    console.log(loader);
+    loader.style.display = 'none';
+    $('body').removeClass('hidden');
+    $('#nav-menu').removeClass('menu-hidden');
+  };
